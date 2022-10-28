ce7a99eacfb3889e56adbb4f9f7549f8ea2b9b95
Matheus Barcelos <barcelosm1996@gmail.com>
2022-10-28 16:50:45 -0300

    Matheus: Algumas alterção na página de Login

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
 import Login from './src/screens/login/Login';
-import Profile from './src/screens/profile/Profile';
 
-export default function App() {
-  return (
-    // <Login/>
-    <Profile/>
-  )
+export default function App(){
+  return <Login/>
 }
--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,86 +1,134 @@
-
+import React from 'react';
+import { Image, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button } from 'react-native-elements';
 import { StatusBar } from 'expo-status-bar';
-import {Button, Image, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useFonts } from "expo-font";
+import { Poppins_700Bold } from "@expo-google-fonts/poppins";
+import { SourceSansPro_700Bold, SourceSansPro_300Light } from "@expo-google-fonts/source-sans-pro";
+
 
 export default function Login() {
 
-return(
-    <>
-<ScrollView>
-<View style={styles.container}>
-  <View style={styles.container2}>
-    <Text style={styles.Text}>Entrar</Text>
-    <Image style={styles.Logo} source={require('../../../assets/logo.png')} />
-    <Text style={styles.TextLogo}>Find My Vehicle</Text>
-  <StatusBar style="auto" />
-  </View>
+  const [font] = useFonts({
+    Poppins_700Bold, SourceSansPro_700Bold, SourceSansPro_300Light
+  })
 
-  <View>
-  <Text style={styles.TextInput}>Email</Text>
-  <TextInput
-    style={styles.input}
-    placeholder="SeuEmail@gmail.com"
-  />
-  <Text style={styles.TextInput}>Senha</Text>
-  <TextInput
-    style={styles.input}
-    placeholder="********************"
-  />
-  <View style={styles.Bt}>
-  <Button 
-  title='Continuar'
-  color={'#19295A'}/>
-  </View>
-  </View>
-</View>
-</ScrollView>
-<Text style={styles.TextVersion}>V1.0</Text>
-  </>
-);
+  if(!font) {
+    return null
+  }
+
+  return(
+    <>
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.container2}>
+            {/*<Image style={styles.logo} source={require('../../../assets/back.png')} />*/}
+            <Text style={styles.page_title}>Entrar</Text>
+            <Image style={styles.logo} source={require('../../../assets/logo.png')} />
+            <Text style={styles.title}>Find My Vehicle</Text>
+          <StatusBar style="auto" />
+          </View>
+
+          <View style={styles.container3}>
+            <Text style={styles.email}>E-mail</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="seuemail@mail.com"
+            />
+            <Text style={styles.psswd}>Senha</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="********************"
+            />
+            <View>
+              <Button
+                  buttonStyle={styles.button}
+                  title='Continuar'
+              />
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+      <Text style={styles.TextVersion}>v1.0</Text>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-container: {
-flex: 1,
-backgroundColor: '#fff',
-alignItems: 'center',
-marginTop: 50,
-},
-container2: {
-marginBottom: 50,
-alignItems: 'center'
-},
-Text: {
-// fontFamily: 'Poppins',
-marginBottom: 100,
-color: '#0D1C4A',
-fontWeight: 'bold',
-fontSize: 22,
-},
-TextInput: {
-fontSize: 16,
-},
-TextLogo:{
-color: '#0D1C4A',
-fontWeight: 'bold',
-fontSize: 25,
-marginBottom: 25,
-},
-input: {
-width: 260,
-borderRadius: 20,
-borderColor:'#FAFAFA',
-height: 60,
-borderWidth: 1,
-padding: 10,
-marginTop:10,
-marginBottom: 10,
-backgroundColor: '#FAFAFA'
-},
-Bt:{
-marginTop: 40,
-},
-TextVersion:{
-alignSelf: 'center'
-}
+  logo: {
+    marginTop: 80,
+    width: 104,
+    height: 104,
+
+    //TODO: Corrigir sombras
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+
+    elevation: 12,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  container2: {
+    marginBottom: 50,
+    alignItems: 'center'
+  },
+  container3: {
+    marginTop: 20,
+  },
+  title: {
+    marginTop: 10,
+    color: '#192959',
+    fontFamily: 'Poppins_700Bold',
+    fontSize: 20,
+  },
+  page_title: {
+    color: '#192959',
+    fontFamily: 'SourceSansPro_700Bold',
+    fontSize: 20,
+  },
+  email: {
+    fontFamily: 'SourceSansPro_300Light',
+    fontSize: 16,
+  },
+  psswd: {
+    fontFamily: 'SourceSansPro_300Light',
+    fontSize: 16,
+    marginTop: 23
+  },
+  TextInput: {
+    borderRadius: 50,
+    fontSize: 16,
+  },
+  input: {
+    width: 273,
+    borderRadius: 50,
+    borderWidth: 1,
+    borderColor:'#FAFAFA',
+    height: 48,
+    paddingHorizontal: 17,
+    marginVertical: 10,
+    backgroundColor: '#FAFAFA'
+  },
+  //TODO: fonte do botão
+  button: {
+    backgroundColor: '#192959',
+    fontSize: 20,
+    height: 48,
+    borderRadius: 50,
+    marginTop: 74,
+  },
+  TextVersion:{
+    color: '#192959',
+    alignSelf: 'center',
+    marginBottom: 10
+  }
 });
